ecb8b97751db35408be72927c8302d056d90ba40
SAT <ts32worker@gmail.com>
2016-08-12 15:58:48 +0900

    画面下部GunPowerメーター仮

---
+++ Assets/src/GunPowerMeter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunPowerMeter : MonoBehaviour {
+
+	public const int MaxGunPower = 1000;
+	public int GunPower = 0;
+
+	/// <summary>
+	/// 残弾ゲージを増減します。
+	/// </summary>
+	public void IncrementGunPower(int value) {
+		if(value < 0) {
+			this.GunPower = 0;
+		} else if(MaxGunPower < value) {
+			this.GunPower = MaxGunPower;
+		} else {
+			this.GunPower = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		var meterObject = GetComponent<UnityEngine.UI.RawImage>();
+		if(meterObject != null) {
+			meterObject.uvRect = new Rect(0, 0, this.GunPower / (float)MaxGunPower, 1.0f);
+			meterObject.transform.localScale = new Vector3(this.GunPower / (float)MaxGunPower, 1, 1);
+		}
+	}
+}
