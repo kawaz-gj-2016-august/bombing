c715f6b0b01bf85cbb9d734e6e363ae76851d0ed
miio <info@miio.info>
2016-08-12 19:16:47 +0900

    敵動きそれっぽいのを

--- Assets/src/SpawnSquare.cs
+++ Assets/src/SpawnSquare.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class SpawnSquare : MonoBehaviour {
 
+	const int MAX_SPAWNED = 500;
 	public GameObject spriteToSpawn;
 	static private int numberSpawned;
 
 	// Use this for initialization
 	void Start () {
 		Debug.Log("Spawner loaded.");
+		spriteToSpawn.transform.position = new Vector3(0, 4.5f, 1);
+		spriteToSpawn.transform.localScale = new Vector3(1, 1, 1);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (numberSpawned < 15) {
-			Instantiate(spriteToSpawn);
+		if (numberSpawned < MAX_SPAWNED) {
+			GameMediator.addEnemy(Instantiate(spriteToSpawn));
 			numberSpawned++;
 		}
 	}
--- Assets/src/SquareMover.cs
+++ Assets/src/SquareMover.cs
@@ -1,20 +1,115 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SquareMover : MonoBehaviour {
+
+	protected float directionX = 0f;
+	protected float directionY = 0f;
+	public float damageRange = 2.0f;
+	protected Vector3 lureStart;
+	protected float lureAngle = 0f;
+	protected float lureStartRange = 0.0f;
+
+	protected int lureFrame = 0;
+	protected int lureLimitFrames = 180;
+	protected bool lureFrameReverse = false;
+
+	protected List<GameObject> usedLures;
+
 
 	// Use this for initialization
 	void Start () {
 
+		// 乱数で方角を確保
+		// X方向は横にいきすぎないように補正をセット
+		directionY = Random.Range(0.05f, 0.18f) * 0.1f;
+		directionX = System.Math.Min(Random.Range(-0.09f, 0.09f) * 0.1f, directionY) - Random.Range(0.000f, 0.005f);
+
+		if (0 > directionX) {
+			directionX = (System.Math.Max(directionX * -0.1f, 0.05f) * -0.1f) + Random.Range(0.000f, 0.005f);
+		}
+
+		// ルアーに入る閾値
+		lureStartRange = Random.Range (3.0f, 5.0f);
+
+		// 使用済みルアーリスト
+		usedLures = new List<GameObject> ();
+
+
 	}
 
 	// Update is called once per frame
-	void Update () {
-		Vector3 pos = this.transform.position;
-		pos.y += 0.05f;
-		//pos.x = Random.Range(-10.0f, 10.0f);
-		this.transform.position = pos;
-		if (pos.y > 3) {
+	void Update()
+	{
+		// ルアーモード
+		bool inLures = false;
+		Vector3 pos = gameObject.transform.position;
+		List<GameObject> lures = GameMediator.getLures();
+
+
+		foreach (GameObject lure in lures)
+		{
+			// ルアーの座標を確保
+			Vector3 lurePos = lure.transform.position;
+			if (!usedLures.Contains(lure) &&					// 使ったことのあるルアーリストにはいない
+				Vector3.Distance(lurePos, pos) < damageRange	// ルアー
+			)
+			{
+				// ルアーに入った
+				inLures = true;
+
+				// ルアー時間を加算
+				lureFrame += 1;
+
+				// ルアー時間が経過いたら、今のルアーから脱出する
+				if (lureFrame >= lureLimitFrames) {
+					lureFrame = 0;
+					//lureFrameReverse = true;
+					usedLures.Add (lure);
+				}
+
+				//if (lureStart == null) {
+				//	lureStart = pos;
+				//}
+
+				Vector3 direction = lurePos - pos;
+				if (Vector3.Distance (lurePos, pos) < lureStartRange) {
+					gameObject.transform.position += direction * 0.01f;
+				}
+
+				// ルアーに入ったので次のルアーにはいかない
+				break;
+			
+				/*
+				// ここにきたら円運動
+				gameObject.transform.position = lurePos + new Vector3(
+					Mathf.Cos(lureAngle + lureStart.x),
+					Mathf.Sin(lureAngle + lureStart.y) ,
+					0);
+				if (lureStart == pos) {
+					inLures = false;
+				}
+				lureAngle += 0.01f;
+				}*/
+
+			}
+
+				
+		}
+
+		// ルアーに入っていなければ通常移動
+		if (!inLures)
+		{
+			pos.y -= directionY;
+			pos.x += directionX;
+			gameObject.transform.position = pos;
+		}
+
+		// 画面したまでいったら破壊
+		if (pos.y < -5)
+		{
+			GameMediator.removeEnemy(gameObject);
 			Destroy(gameObject);
 			SpawnSquare.spriteDestroyed();
 		}
