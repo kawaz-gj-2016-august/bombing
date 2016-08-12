28d2395e2e7eb7e04e1cf2dfd05880f573f94c8f
Collapsed Plug <hsp.tosh.5200113@gmail.com>
2016-08-12 13:41:01 +0900

    Added mediator

---
+++ Assets/SpawnSquare.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSquare : MonoBehaviour {
+
+	public GameObject spriteToSpawn;
+	static private int numberSpawned;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Spawner loaded.");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (numberSpawned < 15) {
+			Instantiate(spriteToSpawn);
+			numberSpawned++;
+		}
+	}
+
+	static public void spriteDestroyed () {
+		numberSpawned--;
+	}
+
+}
