3b80f4bafb1a3074fb4871d5d13b9960bce0d97e
SAT <ts32worker@gmail.com>
2016-08-18 02:17:02 +0900

    スコア加算処理追加・改 使われていない素材を削除

--- Assets/src/MainSceneUI.cs
+++ Assets/src/MainSceneUI.cs
@@ -66,6 +66,7 @@ public class MainSceneUI : MonoBehaviour {
 	// Use this for initialization
 	void Start() {
 		//BGMは AudioSource:BGMSrc の AudioClipに直接セットされることを想定する
+		Application.targetFrameRate = 60;
 	}
 
 	// Update is called once per frame
